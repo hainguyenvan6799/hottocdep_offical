2e757417fc925db863f1a902a2994e0823bd2e9e
Nguyen Van Hai <hainguyenvan6799>
2020-07-22 09:43:59 +0700

    a

--- resources/views/khachhang/khxemlich.blade.php
+++ resources/views/khachhang/khxemlich.blade.php
@@ -55,9 +55,8 @@
 							$delta_minute = 60 - $now_minute;
 						}
 						$total_delta_minute = $delta_hour * 60 + $delta_minute;
-						dd($ld->thoigian . ' ' . $now_hour . ' ' . gettype($ld->thoigian) . ' ' . gettype($now_hour) );
 					?>
-						@if($ld->ngay == $now && $ld->thoigian < $now_hour)
+						@if($ld->ngay == $now && (int)$ld->thoigian < $now_hour)
 							<p class="text-danger">Bạn đã bị lỡ lịch. Nếu bạn đã thanh toán, vui lòng liên hệ admin để hủy lịch và hoàn tiền.</p>
 						@elseif($ld->ngay == $now && $total_delta_minute <= 180)
 							<p>Còn {{$delta_hour}} giờ {{$delta_minute}} phút là đến giờ cắt tóc của quý khách.</p>
